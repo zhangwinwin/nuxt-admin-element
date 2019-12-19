9527533768fdb8147170f9c22e88333439f956db
zhangwinwin <344879340@qq.com>
2019-12-19 11:33:57 +0800

    增加vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,21 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 export const state = () => ({
   // 这两个用于client side的使用, 又放cookie里是为了刷新时状态不丢失
-  hostTableList: []
+  hostTableList: [],
+  paginationConfig: {},
+  hostUrl: '/api/hostList/0'
 })
 
 //  mutation 必须同步执行
 export const mutations = {
   [Types.GETHOSTTABLELIST] (state, value) {
     state.hostTableList = value
+  },
+  [Types.SETPAGINATIONCONFIG] (state, value) {
+    state.paginationConfig = value
+  },
+  [Types.SETHOSTURL] (state, value) {
+    state.hostUrl = value
   }
 }
 
@@ -19,6 +27,12 @@ export const mutations = {
 export const actions = {
   setHostTableList ({commit}, data) {
     commit(Types.GETHOSTTABLELIST, data)
+  },
+  setPaginationConfig ({commit}, data) {
+    commit(Types.SETPAGINATIONCONFIG, data)
+  },
+  setHostUrl ({commit}, data) {
+    commit(Types.SETHOSTURL, data)
   }
 }
 
